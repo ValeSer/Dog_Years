1e9f8ca63af1b2d41eb2336c79181743b9245498
Valeria Serci <balerty84@gmail.com>
2021-03-12 17:55:05 +0000

    calculate the number of dog years

--- app.js
+++ app.js
@@ -8,3 +8,7 @@ earlyYears = earlyYears * 10.5
 
 // we already accounted for the first two years, so we substract 2 to myAge
 let laterYears = myAge - 2
+
+//calculate the number of dog years accounted for by your later years
+//Use the multiplication assignment operator to multiply and assign in one step
+laterYears *= 4;
